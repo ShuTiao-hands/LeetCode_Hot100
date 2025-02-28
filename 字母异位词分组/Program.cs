a260b7fd4137e825728786fdd8d5c5bc34fdac17
ShuTiao-hands <807095386@qq.com>
2025-02-28 14:55:21 +0800

    第二题和第三题上传

---
+++ 字母异位词分组/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, List<string>> mp = new Dictionary<string, List<string>>();
+        foreach (string str in strs)
+        {
+            char[] keyChars = str.ToCharArray(); // 把"eat"拆成 ['e','a','t']
+            Array.Sort(keyChars); // 排序成 ['a','e','t']
+            string key = new string(keyChars); // 合并成 "aet"
+            if (!mp.ContainsKey(key)) // 检查柜子有没有这个标签的抽屉
+            {
+                mp[key] = new List<string>(); // 没有就新增一个抽屉和空盒子
+            }
+            mp[key].Add(str); // 把单词放进对应抽屉的盒子里
+        }
+        return new List<IList<string>>(mp.Values); //mp.Values会返回一个集合包含这个字典中的值
+
+    }
+}
---
+++ 最长连续序列/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int LongestConsecutive(int[] nums)
+    {
+        var set = new HashSet<int>(nums);//声明一个hashset容器，自动去重，声明为int类型避免装箱拆箱
+        int longest = 0;//最连续序列计数
+        foreach (int num in set) //遍历hashset
+        {
+            if (set.Contains(num - 1)) continue;//如果能找到比它小1的数，跳过，说明它不是起点
+            int currentNum = num;//记录起点
+            int currentStreak = 0;//记录序列长度
+            while (set.Contains(currentNum))
+            {
+                currentStreak++;
+                currentNum++;
+            }
+            longest = Math.Max(longest, currentStreak);//比较当前和已记录的，保留更大的
+        }
+        return longest;
+    }
+}
