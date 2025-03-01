1332334fcb232c3167d9b80d9ae250a995513663
ShuTiao-hands <807095386@qq.com>
2025-03-01 09:39:47 +0800

    移动零

---
+++ 移动零/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        //快慢指针
+        int low = 0;
+        int fast = 1;
+        int temp = 0;
+        while(fast < nums.Length)
+        {
+            if (nums[low] == 0)
+            {
+                if (nums[fast] != 0)
+                {
+                    temp = nums[low];
+                    nums[low] = nums[fast];
+                    nums[fast] = temp;
+                }
+                else
+                {
+                    fast++;
+                }
+            }
+            else if(low < fast - 1)
+            {
+                low++;
+            }
+            else
+            {
+                fast++;
+            }
+        }
+    }
+}
