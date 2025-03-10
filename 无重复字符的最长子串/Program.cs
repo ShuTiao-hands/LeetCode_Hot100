6966e15cb0d93eaf4eb0d0736f7645def95faea5
ShuTiao-hands <807095386@qq.com>
2025-03-10 10:54:01 +0800

    无重复字符的最长字串

---
+++ 无重复字符的最长子串/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        int maxNum = 0;
+        int left = 0;
+        Dictionary<char, int> charDic = new Dictionary<char, int>();
+        for (int right = 0; right < s.Length; right++)
+        {
+            if (charDic.ContainsKey(s[right]))
+            {
+                left = Math.Max(left, charDic[s[right]] + 1);
+            }
+            charDic[s[right]] = right;
+            maxNum = Math.Max(maxNum, right - left + 1);
+        }
+        return maxNum;
+
+
+
+    }
+}
